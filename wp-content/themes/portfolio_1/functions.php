a931104ad253e0cc2e3a865e5ca17e7e00980ed6
wenn <wenndycarlier1097@gmail.com>
2019-09-10 14:52:10 +0200

    custom pos type, home

--- wp-content/themes/portfolio_1/functions.php
+++ wp-content/themes/portfolio_1/functions.php
@@ -2,9 +2,50 @@
 //title tag
 add_theme_support('title-tag');
 
+add_theme_support('post-thumbnails');
 
 //menu
 function menu(){
 	register_nav_menu('main_menu', 'Menu principal');
 }
 add_action('init', 'menu');
+
+
+function portfolio_dev() {
+    $labels = array(
+        'name'                  => _x( 'devPortfolios', 'Post type general name', 'textdomain' ),
+        'singular_name'         => _x( 'devPortfolio', 'Post type singular name', 'textdomain' ),
+        'menu_name'             => _x( 'devPortfolios', 'Admin Menu text', 'textdomain' ),
+        'name_admin_bar'        => _x( 'devPortfolio', 'Add New on Toolbar', 'textdomain' ),
+        'add_new'               => __( 'Ajouter un devportfolio', 'textdomain' ),
+        'add_new_item'          => __( 'Ajouter un devportfolio', 'textdomain' ),
+        'new_item'              => __( 'Nouveau devportfolio', 'textdomain' ),
+        'edit_item'             => __( 'Editer devportfolio', 'textdomain' ),
+        'view_item'             => __( 'Voir devportfolio', 'textdomain' ),
+        'all_items'             => __( 'Tous mes devportfolios', 'textdomain' ),
+        'search_items'          => __( 'Chercher les devportfolios', 'textdomain' ),
+        'parent_item_colon'     => __( 'Parent devportfolios:', 'textdomain' ),
+        'not_found'             => __( 'Aucun devportoflio trouvé.', 'textdomain' ),
+        'not_found_in_trash'    => __( 'Aucun devportfolio trouvé dans la corbeille.', 'textdomain' ),
+        
+    );
+ 
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true, 
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'devPortfolio' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 4,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+    );
+ 
+    register_post_type( 'devPortfolio', $args ); 
+}
+ 
+add_action( 'init', 'portfolio_dev' );
--- wp-content/themes/portfolio_1/home.php
+++ wp-content/themes/portfolio_1/home.php
@@ -6,30 +6,81 @@
 get_header();
 ?>
 
-<section>
+<section id="presentation">
     <div class="container-fluid">
-        <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Dévéloppeur/Intégrateur web junior</h1>
+            </div>
+            <div class="col-md-4">
+                <img src="http://placekitten.com/200/200">
+                <p>Prénom - Nom</p>
+                <p>Adresse mail</p>
+                <p>Tel</p>
+                <p>Permis B</p>
+            </div>
+            <div class="col-md-8">
+                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker. </p>
+            </div>
         </div>
     </div>
 </section>
 
-<section>
+<section id="journey">
     <div class="container-fluid">
-        <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Expériences</h2>
+                <p> Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Compétences</h2>
+                <p> Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Formations</h2>
+                <p> Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page</p>
+            </div>
         </div>
     </div>
 </section>
 
-<section>
+<section id="production">
     <div class="container-fluid">
-        <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>Mes réalisations</h2>
+            </div>
+
+            <?php
+            $args = [
+            'post_type'         => 'devPortfolio', 
+            //'posts_per_page'    => 3, 
+            'orderby'           => 'date', //tri par date
+            'order'             => 'DESC' //dans l'ordre descendant
+            ];
+            ?>
+            <?php $ma_boucle = new WP_Query($args); //permet de faire des requetes WP_Query()
+                if( $ma_boucle->have_posts() ) :
+                    while( $ma_boucle->have_posts() ) : $ma_boucle->the_post();
+            ?>
+            <div class="col-md-6">
+                <?php the_post_thumbnail('medium'); ?>
+                <?php the_title(); ?>
+                <?php the_content(); ?>
+            </div>
+            <?php endwhile; endif; ?>
         </div>
     </div>
 </section>
 
-<section>
+<section id="contact_form">
     <div class="container-fluid">
         <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                </div>
+            </div>
         </div>
     </div>
 </section>
